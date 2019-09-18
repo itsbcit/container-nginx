eb2662300eafc4b4c3aa8296dfdbab8e94feedc5
Jesse Weisner <jesse_weisner@bcit.ca>
2019-09-18 16:08:49 -0700

    overwrite existing files

--- 50-copy-config.sh
+++ 50-copy-config.sh
@@ -12,13 +12,13 @@ if [ -d $config_path ]; then
     for f in $(find ${config_path} -maxdepth 1 -type f -name "*.conf");do
         case $(basename $f) in
             default.conf)
-                cp -v $config_path/default.conf ${dest_path}/conf.d/default.conf
+                cp -fv $config_path/default.conf ${dest_path}/conf.d/default.conf
                 ;;
             conf.d-*.conf)
-                cp -v $f $dest_path/conf.d/$(destfilename $f "conf.d")
+                cp -fv $f $dest_path/conf.d/$(destfilename $f "conf.d")
                 ;;
             *)
-                cp -v $f ${dest_path}/
+                cp -fv $f ${dest_path}/
                 ;;
         esac
     done
