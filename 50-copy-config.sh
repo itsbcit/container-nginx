b73f957db95bc5087cf4cba4c18397b296f18031
Jesse Weisner <jesse@weisner.ca>
2023-11-03 11:03:22 -0700

    updated for nginx 1.24 etc layout

--- 50-copy-config.sh
+++ 50-copy-config.sh
@@ -12,10 +12,16 @@ if [ -d $config_path ]; then
     for f in $(find ${config_path} -maxdepth 1 -name "*.conf");do
         case $(basename $f) in
             default.conf)
-                cp -fv $config_path/default.conf ${dest_path}/conf.d/default.conf
+                cp -fv $config_path/default.conf ${dest_path}/http.d/default.conf
                 ;;
             http.d-*.conf)
                 cp -fv $f $dest_path/http.d/$(destfilename $f "http.d")
+                ;;
+            modules-*.conf)
+                cp -fv $f $dest_path/modules/$(destfilename $f "modules")
+                ;;
+            stream.d-*.conf)
+                cp -fv $f $dest_path/stream.d/$(destfilename $f "stream.d")
                 ;;
             conf.d-*.conf)
                 cp -fv $f $dest_path/conf.d/$(destfilename $f "conf.d")
